612019720d269ac74fca94be8970c25b7d130e70
naoto.sasaki <kukandenpan@gmail.com>
2019-05-26 11:13:57 +0000

    modify error

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   belongs_to :user
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites, source: :user
 end
